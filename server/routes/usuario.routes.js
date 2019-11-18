a00785d991cb049febc9b4a412fb73cae0ecd387
Ricardo Gottheil <ricardogottheil@gmail.com>
2019-11-18 09:57:41 -0500

    Nueva configuracion de la BD

--- server/routes/usuario.routes.js
+++ server/routes/usuario.routes.js
@@ -3,10 +3,19 @@ const router = express.Router()
 
 const usuario = require('../controllers/usuario.controller');
 
+// localhost:300/api/usuarios/
 router.get('/', usuario.getUsuarios)
+
+// localhost:300/api/usuarios/
 router.post('/', usuario.createUsuario)
+
+// localhost:300/api/usuarios/:id
 router.get('/:id', usuario.getUsuario)
+
+// localhost:300/api/usuarios/:id
 router.put('/:id', usuario.updateUsuario)
+
+// localhost:300/api/usuarios/:id
 router.delete('/:id', usuario.deleteUsuario)
 
 module.exports = router;
